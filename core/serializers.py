d198d0e82f1119f1c4a4ed75f1761833186462b0
Tim Lukashevich <silvernet@yandex.ru>
2024-10-06 14:18:50 +0300

    store hashed password do not return password from api

--- core/serializers.py
+++ core/serializers.py
@@ -5,3 +5,15 @@ class UserSerializer(ModelSerializer):
   class Meta:
     model = User
     fields = ['id', 'first_name', 'last_name', 'email', 'password'] # '__all__'
+    extra_kwargs = {
+      'password': { 'write_only': True }
+    }
+  
+  def create(self, d):
+    password = d.pop('password', None)
+    instance = self.Meta.model(**d)
+    if password is not None:
+      instance.set_password(password)
+
+    instance.save()
+    return instance

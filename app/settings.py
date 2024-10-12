f8a706ed6c077778e9b41655601675418a7f4d8f
Tim Lukashevich <silvernet@yandex.ru>
2024-10-12 20:19:00 +0300

    fixed http status code for unauthenticated results

--- app/settings.py
+++ app/settings.py
@@ -116,6 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+  'EXCEPTION_HANDLER': 'core.exceptions.status_code_handler'
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/

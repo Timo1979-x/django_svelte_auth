cd2630611914869d5cbc7e2c7de9c6846653b21d
Tim Lukashevich <silvernet@yandex.ru>
2024-10-12 22:40:59 +0300

    retrieve user info

--- svelte-auth/src/pages/Home.svelte
+++ svelte-auth/src/pages/Home.svelte
@@ -1,1 +1,14 @@
-Hello
+<script>
+  import axios from "axios";
+  import { onMount } from "svelte";
+  let message = "You are not logged in";
+
+  onMount(async () => {
+    const { data } = await axios.get("http://localhost:8000/api/user");
+    message = `Hi ${data.first_name} ${data.last_name}`;
+  });
+</script>
+
+<div class="container mt-5 text-center">
+  <h3>{message}</h3>
+</div>

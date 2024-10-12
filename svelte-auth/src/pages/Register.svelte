c7eaa01f50336a4f2c35465ddc2d34895973ccc3
Tim Lukashevich <silvernet@yandex.ru>
2024-10-12 21:43:06 +0300

    added two components

---
+++ svelte-auth/src/pages/Register.svelte
@@ -0,0 +1,29 @@
+<main class="form-signin w-100 m-auto">
+  <form>
+    <h1 class="h3 mb-3 fw-normal">Please register</h1>
+
+    <div class="form-floating">
+      <input class="form-control" id="firstName" placeholder="First Name">
+      <label for="firstName">First Name</label>
+    </div>
+    <div class="form-floating">
+      <input class="form-control" id="lastName" placeholder="Last Name">
+      <label for="lastName">Last Name</label>
+    </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword1" placeholder="Password">
+      <label for="floatingPassword1">Password</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword2" placeholder="Password confirmation">
+      <label for="floatingPassword2">Password confirmation</label>
+    </div>
+
+
+    <button class="btn btn-primary w-100 py-2" type="submit">Submit</button>
+  </form>
+</main>

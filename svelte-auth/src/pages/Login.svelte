3e2b654f96b7a4b2159489361da886df991824b2
Tim Lukashevich <silvernet@yandex.ru>
2024-10-12 22:33:25 +0300

    implemented login

--- svelte-auth/src/pages/Login.svelte
+++ svelte-auth/src/pages/Login.svelte
@@ -1,17 +1,48 @@
+<script>
+  import axios from "axios";
+  import { push } from "svelte-spa-router";
+  let email = "",
+    password = "";
+  $: submit = async () => {
+    const { data } = await axios.post(
+      "http://localhost:8000/api/login",
+      {
+        email,
+        password,
+      },
+      { withCredentials: true },
+    );
+    
+    axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+
+    await push("/");
+  };
+</script>
+
 <main class="form-signin w-100 m-auto">
-  <form>
+  <form on:submit|preventDefault={submit}>
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input
+        bind:value={email}
+        type="email"
+        class="form-control"
+        id="floatingInput"
+        placeholder="name@example.com"
+      />
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
+      <input
+        bind:value={password}
+        type="password"
+        class="form-control"
+        id="floatingPassword1"
+        placeholder="Password"
+      />
+      <label for="floatingPassword1">Password</label>
     </div>
-
-
-    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
   </form>
 </main>

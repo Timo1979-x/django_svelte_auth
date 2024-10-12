951f7126dcd0bce26bfa677c2ab33033a10c8d66
Tim Lukashevich <silvernet@yandex.ru>
2024-10-12 22:20:46 +0300

    register

--- svelte-auth/src/pages/Register.svelte
+++ svelte-auth/src/pages/Register.svelte
@@ -1,28 +1,75 @@
+<script>
+  import axios from "axios";
+  import { push } from "svelte-spa-router";
+  let first_name = "",
+    last_name = "",
+    email = "",
+    password = "",
+    password_confirm = "";
+  $: submit = async () => {
+    await axios.post("http://localhost:8000/api/register", {
+      first_name,
+      last_name,
+      email,
+      password,
+      password_confirm,
+    });
+    await push("/login");
+  };
+</script>
+
 <main class="form-signin w-100 m-auto">
-  <form>
+  <form on:submit|preventDefault={submit}>
     <h1 class="h3 mb-3 fw-normal">Please register</h1>
 
     <div class="form-floating">
-      <input class="form-control" id="firstName" placeholder="First Name">
+      <input
+        bind:value={first_name}
+        class="form-control"
+        id="firstName"
+        placeholder="First Name"
+      />
       <label for="firstName">First Name</label>
     </div>
     <div class="form-floating">
-      <input class="form-control" id="lastName" placeholder="Last Name">
+      <input
+        bind:value={last_name}
+        class="form-control"
+        id="lastName"
+        placeholder="Last Name"
+      />
       <label for="lastName">Last Name</label>
     </div>
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input
+        bind:value={email}
+        type="email"
+        class="form-control"
+        id="floatingInput"
+        placeholder="name@example.com"
+      />
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword1" placeholder="Password">
+      <input
+        bind:value={password}
+        type="password"
+        class="form-control"
+        id="floatingPassword1"
+        placeholder="Password"
+      />
       <label for="floatingPassword1">Password</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword2" placeholder="Password confirmation">
+      <input
+        bind:value={password_confirm}
+        type="password"
+        class="form-control"
+        id="floatingPassword2"
+        placeholder="Password confirmation"
+      />
       <label for="floatingPassword2">Password confirmation</label>
     </div>
-
 
     <button class="btn btn-primary w-100 py-2" type="submit">Submit</button>
   </form>
